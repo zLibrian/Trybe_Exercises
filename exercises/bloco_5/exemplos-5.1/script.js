e45e126ca6951807dbe56a05ce463a6c032e9ecd
Kevin Willian <kevinwillian084@gmail.com>
2021-04-22 12:39:18 -0300

    Resolvendo exercicios bloco 5.1

---
+++ exercises/bloco_5/exemplos-5.1/script.js
@@ -0,0 +1,24 @@
+let title = document.getElementById('page-title');
+title.innerText = 'Get Out';
+title.style.backgroundColor = 'red';
+title.style.textAlign = 'center';
+
+let bodie = document.body; 
+bodie.style.textAlign = 'center';
+bodie.style.backgroundColor = 'green';
+
+
+
+let paragraph = document.getElementById('paragraph');
+paragraph.innerText = 'Olá, gente!!! Essa é minha primeira vez praticando os conhecimentos adquiridos nesse ultimo dia xD';
+paragraph.style.textAlign = 'center';
+
+let subTitle = document.getElementById('subtitle');
+subTitle.innerText = 'O que espero ?';
+subTitle.style.textAlign = 'center';
+
+let secondParagraph = document.getElementById('second-paragraph');
+secondParagraph.innerText = 'Espero em breve conseguir montar um site totalmente dinâmico e interativo xD';
+
+
+
