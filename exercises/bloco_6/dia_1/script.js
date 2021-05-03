daaa874380ec6e41ff913d1141b442139ab8167c
Kevin Willian <kevinwillian084@gmail.com>
2021-05-03 12:49:26 -0300

    Teste

--- exercises/bloco_6/dia_1/script.js
+++ exercises/bloco_6/dia_1/script.js
@@ -1,6 +1,5 @@
-let estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO" ];
 
-
+//Adiciona estados de forma dinamica
 
 for (let index of estados){
   let sectionEstados = document.getElementById('states');
@@ -9,3 +8,6 @@ for (let index of estados){
   createDiv.value = index;
   sectionEstados.appendChild(createDiv);
 }
+let estados = ["AC", "AL", "AM", "AP", "BA", "CE", "DF", "ES", "GO", "MA", "MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN", "RO", "RS", "RR", "SC", "SE", "SP", "TO" ];
+
+console.log(`Primeira vez que uso templates ${estados}`);
