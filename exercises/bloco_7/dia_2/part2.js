2545c32e0c69ad0931df8ef487251b9330271fa9
Kevin Willian <kevinwillian084@gmail.com>
2021-05-10 19:19:27 -0300

    Adiciona todos exercicios parte 2

--- exercises/bloco_7/dia_2/part2.js
+++ exercises/bloco_7/dia_2/part2.js
@@ -26,19 +26,19 @@ const addKey = (obj, key, value) => {
 
 addKey(lesson2, 'turno', 'noite');
 
-// console.log(lesson2);
+console.log(lesson2);
 
 // Crie uma função para listar as keys de um objeto. Essa função deve receber um objeto como parâmetro.
 const listKeys = (object) => Object.keys(object);
-// console.log(listKeys(lesson2));
+console.log(listKeys(lesson2));
 
 // Crie uma função para mostrar o tamanho de um objeto.
 const showObjectSize = (object) => Object.keys(object).length;
-// console.log(showObjectSize(lesson3));
+console.log(showObjectSize(lesson3));
 
 // Crie uma função para listar os valores de um objeto. Essa função deve receber um objeto como parâmetro.
 const listValues = (object) => Object.values(object);
-// console.log(listValues(lesson1));
+console.log(listValues(lesson1));
 
 const allLessons = Object.assign({},  {lesson1, lesson2, lesson3} );
 
@@ -56,8 +56,20 @@ const returnTotalStudents = (object) => {
 }
 
 console.log(returnTotalStudents(allLessons));
+
 // Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
-// Copiar
-// console.log(getValueByNumber(lesson1, 0));
-// // Output: 'Matématica'
-// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave. Exemplo:
+const returnValuesOfKeys = (object, position) => Object.values(object)[position];
+console.log(returnValuesOfKeys(lesson1, 3));
+
+// Crie uma função que verifique se o par (chave / valor) existe na função. Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
+
+const checkIfPairExist = (objeto, nomeChave, valorChave) => {
+const teste = (Object.entries(objeto));
+let isTrue = false;
+for (let index in teste){
+  if (teste[index][0] === nomeChave && teste[index][1] === valorChave) isTrue = true;
+}
+return isTrue;
+};
+
+console.log(checkIfPairExist(lesson2,'materia','História'));
