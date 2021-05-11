f3147a2cdf2c32c0c1e06f5e6140d8ea57f0f6be
Kevin Willian <kevinwillian084@gmail.com>
2021-05-11 16:37:23 -0300

    Adiciona dano do dragao

--- exercises/bloco_8/dia_1/part1.js
+++ exercises/bloco_8/dia_1/part1.js
@@ -21,6 +21,14 @@ const dragon = {
 
 const battleMembers = { mage, warrior, dragon };
 
+//retorna um valor de uma propriedade de um heroi determinado.
+const personagem = (objec, heroe, propriedade) => {
+  return objec[heroe][propriedade];
+}
+console.log(personagem(battleMembers, 'warrior', 'strength'));
+
+// Crie uma função que retorna o dano do dragão.
+// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
 const randomicNumber = (minNumber, maxNumber) => {
   let randomNumber =  Math.ceil(Math.random(minNumber)*maxNumber);
   if (randomNumber < minNumber){
@@ -28,24 +36,18 @@ const randomicNumber = (minNumber, maxNumber) => {
   }
   return randomNumber;
 }
+console.log(randomicNumber(15, personagem(battleMembers, 'warrior', 'strength')));
 
-// Crie uma função que retorna o dano do dragão.
-const returnDamage = (obj, personagem, randomNumber) => {
- let damage = obj[personagem].damage = randomNumber;
-  return damage;
+const returnDamage = (personagem, randomNumber) => {
+  let propriedade = personagem = randomNumber;
+  return propriedade;
 }
 
-console.log(returnDamage(battleMembers, 'dragon', randomicNumber(15, 50)));
-console.log(dragon);
-// const returnDrakeDamage = (object, personagem, propriedade) =>(object[personagem][propriedade]);
-// console.log(returnDrakeDamage(battleMembers, 'dragon', 'healthPoints'));
+const DrakeDamage = returnDamage(personagem(battleMembers, 'dragon', 'strength'), randomicNumber(15, personagem(battleMembers, 'dragon', 'strength')));
+console.log(`O dano do dragao foi de: ${DrakeDamage}`);
 
-// O dano será um número aleatório entre 15 (dano mínimo) e o valor do atributo strength (dano máximo).
-// const damage = returnDrakeDamage(battleMembers, 'dragon', 'strength');
-// 
-// console.log(randomicNumber(15, 50));
-// Crie uma função que retorna o dano causado pelo warrior .
-// O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
-// Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
-// O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
-// A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
+// // Crie uma função que retorna o dano causado pelo warrior .
+// // O dano será um número aleatório entre o valor do atributo strength (dano mínimo) e o valor de strength * weaponDmg (dano máximo).
+// // Crie uma função que retorna um objeto com duas chaves e dois valores contendo o dano e a mana gasta pelo mago em um turno.
+// // O dano será um número aleatório entre o valor do atributo intelligence (dano mínimo) e o valor de intelligence * 2 (dano máximo).
+// // A mana consumida por turno é 15. Além disto a função deve ter uma condicional, caso o mago tenha menos de 15 de mana o valor de dano recebe uma mensagem (Ex: "Não possui mana suficiente") e a mana gasta é 0.
