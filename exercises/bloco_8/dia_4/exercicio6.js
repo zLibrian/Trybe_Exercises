a5a4c3cba11e39a26dd0d9001d2ff6109135a298
Kevin Willian <kevinwillian084@gmail.com>
2021-05-18 21:49:56 -0300

    Exercicio 6 finalizado

---
+++ exercises/bloco_8/dia_4/exercicio6.js
@@ -0,0 +1,22 @@
+const assert = require('assert');
+
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
+
+function studentAverage() {
+  const mapeados = students.map((estudante, index) => ({
+    name: estudante,
+    average: (grades[index].reduce((acc, current) => acc + current, 0) / grades[index].length),
+  }));
+  return mapeados;
+}
+
+console.log(studentAverage());
+
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+assert.deepStrictEqual(studentAverage(), expected);
