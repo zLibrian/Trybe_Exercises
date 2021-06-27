20af00112cf6c26b7c2d0d2b17e90158afe6f5e9
Kevin Willian <kevinwillian084@gmail.com>
2021-06-27 18:25:33 -0300

    Adiciona arquivo Data das imagens]

---
+++ exercises/bloco_11/dia_1/react_exercise_2/src/data.js
@@ -0,0 +1,14 @@
+const catsData = {
+  images: [
+    'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg',
+    ],
+  title: 'Somos gatinhos e rodamos mt',
+};
+
+export default catsData;
