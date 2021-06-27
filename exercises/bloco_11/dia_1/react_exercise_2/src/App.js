a442a2e416fba2bfb7c379e3b83087036101bba5
Kevin Willian <kevinwillian084@gmail.com>
2021-06-27 18:24:41 -0300

    Refatorando exercico 1 para deixalo dinamico

--- exercises/bloco_11/dia_1/react_exercise_2/src/App.js
+++ exercises/bloco_11/dia_1/react_exercise_2/src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Image from './image';
+import catsData from './data';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <div>
-          <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Gatinho' className='App-logo'/>
-          <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Gatinho' className='App-logo'/>
-          <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Gatinho' className='App-logo'/>
-          <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Gatinho' className='App-logo'/>
-          <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Gatinho' className='App-logo'/>
-          <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Gatinho' className='App-logo'/>
-          <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alt='Gatinho' className='App-logo'/>
-        </div>
-    
+        <img src={ logo } className="App-logo" alt="logo" />
+          <Image source={ catsData } alt='Gatinho' className='App-logo' />
       </header>
     </div>
   );
--- exercises/bloco_11/dia_1/react_exercise_2/src/image.js
+++ exercises/bloco_11/dia_1/react_exercise_2/src/image.js
@@ -2,8 +2,22 @@ import React from 'react';
 
 class Image extends React.Component {
   render() {
-    const {source, alt, className} = this.props;
-    return <img src={source} alt={alt} className={className} />;
+    const { source: { images, title }, alt, className } = this.props;
+    return (
+      <section>
+        {images.map((imageLink, index) => (
+          <div>
+          <img 
+            src={ imageLink } 
+            alt={ alt } 
+            className={ className } 
+            key={ index } 
+          />
+          <p>{ title }</p>
+          </div>
+          ))}
+      </section>
+    );
   }
 }
 
