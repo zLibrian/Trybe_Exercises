199bf3b6d990b0fdeeeb1eddf5da463c3eea809e
Kevin <kevinwillian084@gmail.com>
2022-05-06 18:02:51 -0300

    remove todos os exercicios do modulo de backend

--- exercises/back_end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-2-nodejs-fluxo-assincrono/io-exemples/writeFile.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs').promises;
-
-fs.writeFile('./meuArquivo.txt', "meu texto de cu")
-  .then(() => console.log("Arquivo de cu"))
-  .catch((err) => console.log(err))
--- exercises/back_end/bloco-23-nodejs-camada-de-servico-e-arquitetura-rest-e-restful/models_example/helpers/getNewAuthor.js
+++
@@ -1,6 +0,0 @@
-const getNewAuthor = ({ name, middleName, lastName }) => {
-  const fullName = [name, middleName, lastName].filter((n) => n).join(' ');
-  return fullName;
-};
-
-module.exports = getNewAuthor;
